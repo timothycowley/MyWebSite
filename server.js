8cf82240a9caefa667074f39962693be496770e9
tcowley <cowley.t@husky.neu.edu>
2016-04-28 00:18:23 -0700

    added routing

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var app = express();
 
 app.use(express.static(__dirname));
 
+//changed the above to this
+// app.use(function(req, res) {
+// 		res.sendfile(__dirname + '/index.html');
+// });
+
 app.listen(80);
 console.log('Listening on port 80');
 
